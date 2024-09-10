8c6c14a5908ee59a5a5e8857f5fa659e6f3b153d
brennmu <brenster863@gmail.com>
2024-09-10 18:50:09 -0500

    adding program1.py

---
+++ program1.py
@@ -0,0 +1,4 @@
+first_name = input("What is your first name: ")
+last_name = input("What is your last name: ")
+user_name = last_name.lower() + first_name.lower() + "83"
+print(f"Your generated user name is ", user_name)
