ca7890ff0ba82cb6acc7512b43f025da308ca00a
brennmu <143474487+brennmu@users.noreply.github.com>
2024-09-10 18:55:56 -0500

    Updated program1.py line 1

--- program1.py
+++ program1.py
@@ -1,4 +1,4 @@
-first_name = input("What is your first name: ")
+first_name = input("What's your first name: ")
 last_name = input("What is your last name: ")
 user_name = last_name.lower() + first_name.lower() + "83"
 print(f"Your generated user name is ", user_name)
